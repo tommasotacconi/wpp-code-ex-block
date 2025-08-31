74f4de46dc31e20d7f6e74a92004eaed28d2fb8c
tommasoTacconi <tommaso.tacconi@gmail.com>
2025-08-31 02:39:05 +0200

    chore: Rerun build after implementing RichText

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks'), 'version' => 'c47ec9476b24ef6b0b10');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks'), 'version' => 'a66f38954f46b57a012b');
