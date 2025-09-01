a8d59c0b5741e623a6b6a0548ad71d5a45bdb02a
tommasoTacconi <tommaso.tacconi@gmail.com>
2025-09-01 15:50:00 +0200

    fix: Registered the meta field 'codex_content' and fixed attributes with right name

--- cod-ex-block.php
+++ cod-ex-block.php
@@ -1,10 +1,20 @@
 <?php
+
 /**
  * Plugin Name: Coding example code block
  * Description: A custom code block with consistent styling but editable content per page.
  * Version: 1.0
  * Author: Tommaso Tacconi
  */
+
+// Register the meta field
+add_action('init', function () {
+	register_post_meta('', 'codex_content', array(
+		'show_in_rest' => true,
+		'single' => true,
+		'type' => 'string'
+	));
+});
 
 function cod_ex_block_register()
 {
