a32b08c87ed8eea02bf87c702b7216c6ac80125a
tommasoTacconi <tommaso.tacconi@gmail.com>
2025-09-01 14:59:25 +0200

    build: Run build after fix

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks'), 'version' => 'a66f38954f46b57a012b');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks'), 'version' => 'b159504fdfe50b521b47');
